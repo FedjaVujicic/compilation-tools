3b1dc151710d91213e871317e1ead16449375ab6
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2023-12-20 15:15:30 +0100

    Add stack test and lexing error lineno

--- tests/emulator-tests/start.sh
+++ tests/emulator-tests/start.sh
@@ -2,9 +2,9 @@ ASSEMBLER=assembler
 LINKER=linker
 EMULATOR=emulator
 
-${ASSEMBLER} -o test6.o emulator-tests/test6.s
+${ASSEMBLER} -o test4.o emulator-tests/test4.s
 ${LINKER} -hex \
   -place=my_code@0x40000000 \
   -o program.hex \
-  test6.o
+  test4.o
 ${EMULATOR} program.hex
--- tests/emulator-tests/test4.s
+++ tests/emulator-tests/test4.s
@@ -8,8 +8,14 @@ ld $handler, %r1
 csrwr %r1, %handler
 ld $7, %r13
 csrwr %r13, %status
+st %pc, pc_old1
 
 int
+
+st %r10, pc_old2
+
+ld pc_old1, %r9
+ld pc_old2, %r10
 
 call func1
 ld $0x22222222, %r3
@@ -23,6 +29,7 @@ ld $0x55555555, %r5
 halt
 
 handler:
+ld [%sp + 0], %r10
 ld $0x11111111, %r1
 csrrd %status, %r12
 csrrd %cause, %r13
@@ -39,5 +46,11 @@ bgt %r3, %r2, my_start2
 func3:
 ld $0x44444444, %r4
 jmp my_start3
+
+pc_old1:
+.word 0
+
+pc_old2:
+.word 0
 
 .end
